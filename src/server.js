1742865f064c884e8016707b4952521b737e9017
Bhavin Agarwal <bhavib.789@gmail.com>
2019-12-02 23:36:19 +0530

    Add Mongoose connection and query to fetch people and person on the basis of ID, WIP

--- src/server.js
+++ src/server.js
@@ -1,100 +1,144 @@
-import { graphql, buildSchema } from "graphql";
+import {
+  graphql,
+  buildSchema,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema
+} from "graphql";
 import express from "express";
 import graphqlHTTP from "express-graphql";
-import Mongosoe from "mongoose";
+import Mongoose from "mongoose";
 
-var schema = buildSchema(`
-  type Query {
-    user(id: Int!): Person
-    users(gender: String): [Person] 
-  },
-  type Mutation{
-    updateUser(id: Int!, age: Int!): Person
+Mongoose.connect("mongodb://localhost/graphqltutorial");
+
+const PersonModel = Mongoose.model("person", {
+  firstName: String,
+  lastName: String
+});
+
+const PersonType = new GraphQLObjectType({
+  name: "Person",
+  fields: {
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString }
   }
-  type Person {
-    id: Int
-    name: String
-    gender: String
-    age: Int
-  }
-`);
+});
 
-const users = [
-  {
-    id: 1,
-    name: "Brian",
-    age: "21",
-    gender: "M"
-  },
-  {
-    id: 2,
-    name: "Kim",
-    age: "22",
-    gender: "M"
-  },
-  {
-    id: 3,
-    name: "Joseph",
-    age: "23",
-    gender: "M"
-  },
-  {
-    id: 3,
-    name: "Faith",
-    age: "23",
-    gender: "F"
-  },
-  {
-    id: 5,
-    name: "Joy",
-    age: "25",
-    gender: "F"
-  }
-];
-
-/**
- * Query Resolver functions
- */
-
-const getUser = args => {
-  const userId = args.id;
-  return users.filter(user => user.id === userId)[0];
-};
-const getUsers = args => {
-  if (args.gender) {
-    const gender = args.gender;
-    return users.filter(user => user.gender === gender);
-  } else {
-    return users;
-  }
-};
-
-/**
- * Mutation Resolver function
- */
-
-const updateUser = ({ id, age }) => {
-  users.map(user => {
-    if (user.id === id) {
-      user.age = age;
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      people: {
+        type: GraphQLList(PersonType),
+        resolve: (root, args, context, info) => {
+          return PersonModel.find().exec();
+        }
+      },
+      person: {
+        type: PersonType,
+        resolve: (root, args, context, info) => {
+          return PersonType.findById(args.id).exec();
+        }
+      }
     }
-  });
+  })
+});
 
-  return users.filter(user => user.id === id)[0];
-};
+// var schema = buildSchema(`
+//   type Query {
+//     user(id: Int!): Person
+//     users(gender: String): [Person]
+//   },
+//   type Mutation{
+//     updateUser(id: Int!, age: Int!): Person
+//   }
+//   type Person {
+//     id: Int
+//     name: String
+//     gender: String
+//     age: Int
+//   }
+// `);
 
-var root = {
-  user: getUser,
-  users: getUsers,
-  updateUser: updateUser
-};
+// const users = [
+//   {
+//     id: 1,
+//     name: "Brian",
+//     age: "21",
+//     gender: "M"
+//   },
+//   {
+//     id: 2,
+//     name: "Kim",
+//     age: "22",
+//     gender: "M"
+//   },
+//   {
+//     id: 3,
+//     name: "Joseph",
+//     age: "23",
+//     gender: "M"
+//   },
+//   {
+//     id: 3,
+//     name: "Faith",
+//     age: "23",
+//     gender: "F"
+//   },
+//   {
+//     id: 5,
+//     name: "Joy",
+//     age: "25",
+//     gender: "F"
+//   }
+// ];
+
+// /**
+//  * Query Resolver functions
+//  */
+
+// const getUser = args => {
+//   const userId = args.id;
+//   return users.filter(user => user.id === userId)[0];
+// };
+// const getUsers = args => {
+//   if (args.gender) {
+//     const gender = args.gender;
+//     return users.filter(user => user.gender === gender);
+//   } else {
+//     return users;
+//   }
+// };
+
+// /**
+//  * Mutation Resolver function
+//  */
+
+// const updateUser = ({ id, age }) => {
+//   users.map(user => {
+//     if (user.id === id) {
+//       user.age = age;
+//     }
+//   });
+
+//   return users.filter(user => user.id === id)[0];
+// };
+
+// var root = {
+//   user: getUser,
+//   users: getUsers,
+//   updateUser: updateUser
+// };
 
 var app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true
   })
 );
