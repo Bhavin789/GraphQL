fe0fdcf7e1563346571192b3782408d2cc32937f
Bhavin Agarwal <bhavib.789@gmail.com>
2019-12-07 14:22:37 +0530

    Make user schema and queries, WIP

--- src/server.js
+++ src/server.js
@@ -12,146 +12,146 @@ import express from "express";
 import graphqlHTTP from "express-graphql";
 import Mongoose from "mongoose";
 
-Mongoose.connect("mongodb://localhost/graphqltutorial");
+// Mongoose.connect("mongodb://localhost/graphqltutorial");
 
-const PersonModel = Mongoose.model("person", {
-  firstname: String,
-  lastname: String
-});
+// const PersonModel = Mongoose.model("person", {
+//   firstname: String,
+//   lastname: String
+// });
 
-const PersonType = new GraphQLObjectType({
-  name: "Person",
-  fields: {
-    id: { type: GraphQLID },
-    firstname: { type: GraphQLString },
-    lastname: { type: GraphQLString }
-  }
-});
-
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Query",
-    fields: {
-      people: {
-        type: GraphQLList(PersonType),
-        resolve: (root, args, context, info) => {
-          return PersonModel.find().exec();
-        }
-      },
-      person: {
-        type: PersonType,
-        args: {
-          id: { type: GraphQLNonNull(GraphQLID) }
-        },
-        resolve: (root, args, context, info) => {
-          return PersonModel.findById(args.id).exec();
-        }
-      }
-    }
-  }),
-  mutation: new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-      person: {
-        type: PersonType,
-        args: {
-          firstname: { type: GraphQLNonNull(GraphQLString) },
-          lastname: { type: GraphQLNonNull(GraphQLString) }
-        },
-        resolve: (root, args, context, info) => {
-          var person = new PersonModel(args);
-          return person.save();
-        }
-      }
-    }
-  })
-});
-
-// var schema = buildSchema(`
-//   type Query {
-//     user(id: Int!): Person
-//     users(gender: String): [Person]
-//   },
-//   type Mutation{
-//     updateUser(id: Int!, age: Int!): Person
+// const PersonType = new GraphQLObjectType({
+//   name: "Person",
+//   fields: {
+//     id: { type: GraphQLID },
+//     firstname: { type: GraphQLString },
+//     lastname: { type: GraphQLString }
 //   }
-//   type Person {
-//     id: Int
-//     name: String
-//     gender: String
-//     age: Int
-//   }
-// `);
+// });
 
-// const users = [
-//   {
-//     id: 1,
-//     name: "Brian",
-//     age: "21",
-//     gender: "M"
-//   },
-//   {
-//     id: 2,
-//     name: "Kim",
-//     age: "22",
-//     gender: "M"
-//   },
-//   {
-//     id: 3,
-//     name: "Joseph",
-//     age: "23",
-//     gender: "M"
-//   },
-//   {
-//     id: 3,
-//     name: "Faith",
-//     age: "23",
-//     gender: "F"
-//   },
-//   {
-//     id: 5,
-//     name: "Joy",
-//     age: "25",
-//     gender: "F"
-//   }
-// ];
-
-// /**
-//  * Query Resolver functions
-//  */
-
-// const getUser = args => {
-//   const userId = args.id;
-//   return users.filter(user => user.id === userId)[0];
-// };
-// const getUsers = args => {
-//   if (args.gender) {
-//     const gender = args.gender;
-//     return users.filter(user => user.gender === gender);
-//   } else {
-//     return users;
-//   }
-// };
-
-// /**
-//  * Mutation Resolver function
-//  */
-
-// const updateUser = ({ id, age }) => {
-//   users.map(user => {
-//     if (user.id === id) {
-//       user.age = age;
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: "Query",
+//     fields: {
+//       people: {
+//         type: GraphQLList(PersonType),
+//         resolve: (root, args, context, info) => {
+//           return PersonModel.find().exec();
+//         }
+//       },
+//       person: {
+//         type: PersonType,
+//         args: {
+//           id: { type: GraphQLNonNull(GraphQLID) }
+//         },
+//         resolve: (root, args, context, info) => {
+//           return PersonModel.findById(args.id).exec();
+//         }
+//       }
 //     }
-//   });
+//   }),
+//   mutation: new GraphQLObjectType({
+//     name: "Mutation",
+//     fields: {
+//       person: {
+//         type: PersonType,
+//         args: {
+//           firstname: { type: GraphQLNonNull(GraphQLString) },
+//           lastname: { type: GraphQLNonNull(GraphQLString) }
+//         },
+//         resolve: (root, args, context, info) => {
+//           var person = new PersonModel(args);
+//           return person.save();
+//         }
+//       }
+//     }
+//   })
+// });
 
-//   return users.filter(user => user.id === id)[0];
-// };
+var schema = buildSchema(`
+  type Query {
+    user(id: Int!): Person
+    users(gender: String): [Person]
+  },
+  type Mutation{
+    updateUser(id: Int!, age: Int!): Person
+  }
+  type Person {
+    id: Int
+    name: String
+    gender: String
+    age: Int
+  }
+`);
 
-// var root = {
-//   user: getUser,
-//   users: getUsers,
-//   updateUser: updateUser
-// };
+const users = [
+  {
+    id: 1,
+    name: "Brian",
+    age: "21",
+    gender: "M"
+  },
+  {
+    id: 2,
+    name: "Kim",
+    age: "22",
+    gender: "M"
+  },
+  {
+    id: 3,
+    name: "Joseph",
+    age: "23",
+    gender: "M"
+  },
+  {
+    id: 3,
+    name: "Faith",
+    age: "23",
+    gender: "F"
+  },
+  {
+    id: 5,
+    name: "Joy",
+    age: "25",
+    gender: "F"
+  }
+];
+
+/**
+ * Query Resolver functions
+ */
+
+const getUser = args => {
+  const userId = args.id;
+  return users.filter(user => user.id === userId)[0];
+};
+const getUsers = args => {
+  if (args.gender) {
+    const gender = args.gender;
+    return users.filter(user => user.gender === gender);
+  } else {
+    return users;
+  }
+};
+
+/**
+ * Mutation Resolver function
+ */
+
+const updateUser = ({ id, age }) => {
+  users.map(user => {
+    if (user.id === id) {
+      user.age = age;
+    }
+  });
+
+  return users.filter(user => user.id === id)[0];
+};
+
+var root = {
+  user: getUser,
+  users: getUsers,
+  updateUser: updateUser
+};
 
 var app = express();
 app.use(
